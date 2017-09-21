4ae6cb6c93b2341dc7e7d8561ed450f3a209d5fb
eagleEggs <eric.mitchell.r@gmail.com>
2017-09-21 16:47:22 -0400

    Update cavePeopleAgent.cs

--- cavePeople/scripts/world/cavePeopleAgent.cs
+++ cavePeople/scripts/world/cavePeopleAgent.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 public class cavePeople : Agent
 {
-    [Header("eagleEggs/cavePeople")]
+    [Header("eagleEggs/UML/cavePeople")]
     public cavePeopleAcademy academy;
 
     GameObject trueAgent;
@@ -13,9 +19,10 @@ public class cavePeople : Agent
     {
         trueAgent = gameObject;
 
-        public bool isRaining;
-        public bool hasStick;
-        public bool isOutside;
+        public static bool isRaining;
+        public static bool hasStick;
+        public static bool isOutside;
+        public static GameObject stick;
 
     }
 
@@ -31,16 +38,16 @@ public class cavePeople : Agent
             if (actor.tag == "agent")
             {
                 agent = actor;
-                state.Add(actor.transform.position.x / 1);
-                state.Add(actor.transform.position.z / 1);
+                state.Add(actor.transform.position.x / 10);
+                state.Add(actor.transform.position.z / 10);
                 continue;
             }
         }
         
-        state.Add(currentClosestGoal.transform.position.x / 1);
-        state.Add(currentClosestGoal.transform.position.z / 1);
-        state.Add(currentClosestPit.transform.position.x / 1);
-        state.Add(currentClosestPit.transform.position.z / 1);
+        state.Add(currentClosestGoal.transform.position.x / 10);
+        state.Add(currentClosestGoal.transform.position.z / 10);
+        state.Add(currentClosestPit.transform.position.x / 10);
+        state.Add(currentClosestPit.transform.position.z / 10);
 
         return state;
     }
@@ -55,37 +62,37 @@ public class cavePeople : Agent
         // 0 - Forward, 1 - Backward, 2 - Left, 3 - Right
         if (action == 3)
         {
-            Collider[] checkWall = Physics.OverlapBox(new Vector3(trueAgent.transform.position.x + 1, 0, trueAgent.transform.position.z), new Vector3(0.3f, 0.3f, 0.3f));
+            Collider[] checkWall = Physics.OverlapBox(new Vector3(trueAgent.transform.position.x + 0.2f, 0, trueAgent.transform.position.z), new Vector3(0.3f, 0.3f, 0.3f));
             if (checkWall.Where(col => col.gameObject.tag == "wall").ToArray().Length == 0)
             {
-                trueAgent.transform.translate = new Vector3(trueAgent.transform.position.x + 1, 0, trueAgent.transform.position.z);
+                trueAgent.transform.translate = new Vector3(trueAgent.transform.position.x + 0.2f, 0, trueAgent.transform.position.z);
             }
         }
 
         if (action == 2)
         {
-            Collider[] checkWall = Physics.OverlapBox(new Vector3(trueAgent.transform.position.x - 1, 0, trueAgent.transform.position.z), new Vector3(0.3f, 0.3f, 0.3f));
+            Collider[] checkWall = Physics.OverlapBox(new Vector3(trueAgent.transform.position.x - 0.2f, 0, trueAgent.transform.position.z), new Vector3(0.3f, 0.3f, 0.3f));
             if (checkWall.Where(col => col.gameObject.tag == "wall").ToArray().Length == 0)
             {
-                trueAgent.transform.translate = new Vector3(trueAgent.transform.position.x - 1, 0, trueAgent.transform.position.z);
+                trueAgent.transform.translate = new Vector3(trueAgent.transform.position.x - 0.2f, 0, trueAgent.transform.position.z);
             }
         }
 
         if (action == 0)
         {
-            Collider[] checkWall = Physics.OverlapBox(new Vector3(trueAgent.transform.position.x, 0, trueAgent.transform.position.z + 1), new Vector3(0.3f, 0.3f, 0.3f));
+            Collider[] checkWall = Physics.OverlapBox(new Vector3(trueAgent.transform.position.x, 0, trueAgent.transform.position.z + 0.2f), new Vector3(0.3f, 0.3f, 0.3f));
             if (checkWall.Where(col => col.gameObject.tag == "wall").ToArray().Length == 0)
             {
-                trueAgent.transform.translate = new Vector3(trueAgent.transform.position.x, 0, trueAgent.transform.position.z + 1);
+                trueAgent.transform.translate = new Vector3(trueAgent.transform.position.x, 0, trueAgent.transform.position.z + 0.2f);
             }
         }
 
         if (action == 1)
         {
-            Collider[] checkWall = Physics.OverlapBox(new Vector3(trueAgent.transform.position.x, 0, trueAgent.transform.position.z - 1), new Vector3(0.3f, 0.3f, 0.3f));
+            Collider[] checkWall = Physics.OverlapBox(new Vector3(trueAgent.transform.position.x, 0, trueAgent.transform.position.z - 0.2f), new Vector3(0.3f, 0.3f, 0.3f));
             if (checkWall.Where(col => col.gameObject.tag == "wall").ToArray().Length == 0)
             {
-                trueAgent.transform.translate = new Vector3(trueAgent.transform.position.x, 0, trueAgent.transform.position.z - 1);
+                trueAgent.transform.translate = new Vector3(trueAgent.transform.position.x, 0, trueAgent.transform.position.z - 0.2f);
             }
         }
 
@@ -99,7 +106,12 @@ public class cavePeople : Agent
         if (hitObjects.Where(col => col.gameObject.tag == "stick").ToArray().Length == 1)
         {
             if(isRaining==false){
-            reward = 1f;} else {reward = +0.1f;}
+            reward = 1f;hasStick=true;}
+            
+                else {  reward = +0.1f;
+                        hasStick=true;
+                        cavePepleManager.stick.SetActive(false);
+                    }
             done = true;
         }
         if (hitObjects.Where(col => col.gameObject.tag == "fire").ToArray().Length == 1)
